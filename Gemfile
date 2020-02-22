9768124e6d5216728d62776faf0b2e08935ed5d7
Watayo <ryo0616mani@gmail.com>
2020-02-22 21:26:28 +0900

    first commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+ruby '2.6.2'
+source "https://rubygems.org"
+
+gem 'rubocop'
+gem 'ruby-debug-ide'
+gem 'debase'
+gem 'rcodetools'
+gem 'fastri'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+
+gem 'sinatra-websocket'
---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+
+require 'sinatra-websocket'
+
+set :server, 'thin'
+set :sockets, []
+
+get '/' do
+  erb :index
+end
+
+get '/websocket' do
+  if request.websocket?
+    request.websocket do |ws|
+      ws.onopen do
+        settings.sockets << ws
+      end
+      ws.onmessage do |msg|
+        settings.sockets.each do |s|
+          s.send(msg)
+        end
+      end
+      ws.onclose do
+        settings.sockets.delete(ws)
+      end
+    end
+  end
+end
